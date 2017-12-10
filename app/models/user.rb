6de7e8074746d2e3e5478dbef128f65de4631539
Lisa Lee <snickers495@gmail.com>
2017-12-10 02:21:25 +0000

    working on controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   has_many :rides
   has_many :attractions, through: :rides
   validates :password, presence: true
+  enum role: [:normal, :admin]
 
   def mood
     if self.happiness > self.nausea
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
 
   resources :attractions
   resources :users
+  get "/", to: "users#new"
 end
